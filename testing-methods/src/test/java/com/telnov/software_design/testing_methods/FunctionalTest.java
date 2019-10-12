03806840ec62e0da49716ca998d565f65007091a
sergeitelnow <sergeitelnow@gmail.com>
2019-10-13 00:23:37 +0300

    [testing-methods] add main

---
+++ testing-methods/src/test/java/com/telnov/software_design/testing_methods/FunctionalTest.java
@@ -0,0 +1,10 @@
+package com.telnov.software_design.testing_methods;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+abstract class FunctionalTest {
+}
